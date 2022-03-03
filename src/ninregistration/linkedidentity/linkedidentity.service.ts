b655e9e9b9f617edae58d31458f98850cc099a83
timi-o3 <86927745+timi-o3@users.noreply.github.com>
2022-03-03 21:51:57 +0100

    ZUMBA

--- src/ninregistration/linkedidentity/linkedidentity.service.ts
+++ src/ninregistration/linkedidentity/linkedidentity.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Biodatum } from '../biodata/entities/biodatum.entity';
 import { CreateLinkedidentityDto } from './dto/create-linkedidentity.dto';
 import { UpdateLinkedidentityDto } from './dto/update-linkedidentity.dto';
 import { Linkedidentity } from './entities/linkedidentity.entity';
